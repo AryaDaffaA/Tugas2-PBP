054f7e45f217fc6efba79934aadd3e413533da37
arya.daffa1 <arya.daffa@gmail.com>
2022-09-14 19:21:57 +0700

    update tugas 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from katalog.models import CatalogItem
 # TODO: Create your views here.
+def show_catalog(request):
+    data_catalog = CatalogItem.objects.all()
+    subject = {
+        'list_barang': data_catalog,
+        'nama': 'Arya Daffa',
+        'NPM': '2106650853',
+    }  
+    return render(request, "katalog.html", subject)
