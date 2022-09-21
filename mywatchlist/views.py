f8019056097f57292a959e262fa6138d56d2eb06
arya.daffa1 <arya.daffa@gmail.com>
2022-09-21 19:21:16 +0700

    update tugas-3 pbp bonus

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -6,10 +6,19 @@ from django.core import serializers
 # TODO: Create your views here.
 def watchlist(request):
     data_watchlist = MyWatchList.objects.all()
+    counter = 0
+    for movies in data_watchlist:
+        if(movies.watched):
+            counter+=1
+    if(counter < 10 - counter):
+        pesan = "Wah, kamu masih sedikit menonton!"
+    else:
+        pesan = "Selamat, kamu sudah banyak menonton!"
     subject = {
         'myWatchList': data_watchlist,
         'nama': 'Arya Daffa',
         'NPM': '2106650853',
+        'message' : pesan
     }  
     return render(request, "watchlist.html", subject)
 
@@ -28,3 +37,4 @@ def id_funct_xml(request, id):
 def id_funct_json(request, id):
     data = MyWatchList.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
