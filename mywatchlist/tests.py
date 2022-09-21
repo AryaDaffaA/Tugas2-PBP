f1d3b0cb427b66f748260cae52bbca122cd1244d
arya.daffa1 <arya.daffa@gmail.com>
2022-09-21 19:16:32 +0700

    test.py

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,18 +1,17 @@
 from django.test import TestCase, Client
 from django.urls import resolve
-from .views import index
 
 class ContohAppTest(TestCase):
-    def test_contoh_app_url_exist(self):
+    def test_html_url_exist(self):
         response = Client().get('https://tugas2-pbp-arya.herokuapp.com/mywatchlist/')
-        self.asserEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
-    def test_contoh_app_url_exist(self):
-        response = Client().get('/contohapp')
-        self.asserEqual(response.status_code, 200)
+    def test_xml_url_exist(self):
+        response = Client().get('https://tugas2-pbp-arya.herokuapp.com/mywatchlist/xml/')
+        self.assertEqual(response.status_code, 200)
 
-    def test_contoh_app_url_exist(self):
-        response = Client().get('/contohapp')
-        self.asserEqual(response.status_code, 200)
+    def test_json_url_exist(self):
+        response = Client().get('https://tugas2-pbp-arya.herokuapp.com/mywatchlist/json/')
+        self.assertEqual(response.status_code, 200)
 
 # Create your tests here.
